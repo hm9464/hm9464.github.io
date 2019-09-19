46e5fc447fef76806240c192d9fb628486749360
hmakharia <himanshu.makharia@grantsolutions.gov>
2019-09-18 22:22:36 -0400

    updated homepage

--- copy_files_to_ec2.sh
+++ copy_files_to_ec2.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-scp -i ~/.ssh/hm.pem -r site ubuntu@52.200.221.61:~/
-
+scp -i ~/.ssh/hm.pem -r site/css ubuntu@52.200.221.61:~/site/
+scp -i ~/.ssh/hm.pem -r site/images ubuntu@52.200.221.61:~/site/
+scp -i ~/.ssh/hm.pem -r site/maps ubuntu@52.200.221.61:~/site/
+scp -i ~/.ssh/hm.pem -r site/pages ubuntu@52.200.221.61:~/site/
+scp -i ~/.ssh/hm.pem -r site/plots ubuntu@52.200.221.61:~/site/
+scp -i ~/.ssh/hm.pem -r site/index.html ubuntu@52.200.221.61:~/site/
