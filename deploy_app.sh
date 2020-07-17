a7edce36ca4a38de16a11b438ebab0b9c662bd83
himi64 <himi64@hotmail.com>
2020-07-17 00:13:16 -0400

    added deployment script

--- deploy_app.sh
+++ deploy_app.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
+echo "Copying files from local server to EC2"
 scp -i ~/.ssh/hm.pem -r site/css ubuntu@52.200.221.61:~/site/
 scp -i ~/.ssh/hm.pem -r site/images ubuntu@52.200.221.61:~/site/
 scp -i ~/.ssh/hm.pem -r site/maps ubuntu@52.200.221.61:~/site/
 scp -i ~/.ssh/hm.pem -r site/pages ubuntu@52.200.221.61:~/site/
 scp -i ~/.ssh/hm.pem -r site/plots ubuntu@52.200.221.61:~/site/
 scp -i ~/.ssh/hm.pem -r site/index.html ubuntu@52.200.221.61:~/site/
+
+echo "Moving files within the EC2 instances"
+ssh -i ~/.ssh/hm.pem ubuntu@52.200.221.61 "bash move_files.sh"
+
+echo "Restarting server"
+ssh -i ~/.ssh/hm.pem ubuntu@52.200.221.61 "bash restart_server.sh"
+echo "Deployment Complete!"
+
